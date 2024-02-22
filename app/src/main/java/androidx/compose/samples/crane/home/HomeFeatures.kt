2d5c096163dabf553a60c47724d605aa02892705
Adrrien04 <adrrien.chandrakumar@gmail.com>
2024-02-22 22:26:22 +0100

    modif style

--- app/src/main/java/androidx/compose/samples/crane/home/HomeFeatures.kt
+++ app/src/main/java/androidx/compose/samples/crane/home/HomeFeatures.kt
@@ -1,23 +1,13 @@
 /*
+ * Crane Search Content
  * Copyright 2020 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Licensed under the Apache License, Version 2.0
+ * https://www.apache.org/licenses/LICENSE-2.0
  */
 
 package androidx.compose.samples.crane.home
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyGridScope
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
@@ -43,31 +33,32 @@ fun FlySearchContent(
         else -> 1
     }
 
-    CraneSearch(
-        columns,
-        content = {
-            item {
-                PeopleUserInput(
-                    titleSuffix = ", Economy",
-                    onPeopleChanged = searchUpdates.onPeopleChanged
-                )
-            }
-            item {
-                FromDestination()
-            }
-            item {
-                ToDestinationUserInput(
-                    onToDestinationChanged = searchUpdates.onToDestinationChanged
-                )
-            }
-            item {
-                DatesUserInput(
-                    datesSelected,
-                    onDateSelectionClicked = searchUpdates.onDateSelectionClicked
-                )
-            }
+    CraneSearch(columns) {
+        // People user input
+        item {
+            PeopleUserInput(
+                titleSuffix = ", Economy",
+                onPeopleChanged = searchUpdates.onPeopleChanged
+            )
         }
-    )
+
+        item {
+            FromDestination()
+        }
+
+        item {
+            ToDestinationUserInput(
+                onToDestinationChanged = searchUpdates.onToDestinationChanged
+            )
+        }
+
+        item {
+            DatesUserInput(
+                datesSelected,
+                onDateSelectionClicked = searchUpdates.onDateSelectionClicked
+            )
+        }
+    }
 }
 
 @Composable
@@ -76,32 +67,35 @@ fun SleepSearchContent(
     datesSelected: String,
     sleepUpdates: SleepSearchContentUpdates
 ) {
+
     val columns = when (widthSize) {
         WindowWidthSizeClass.Compact -> 1
         WindowWidthSizeClass.Medium -> 3
         WindowWidthSizeClass.Expanded -> 3
         else -> 1
     }
-    CraneSearch(
-        columns,
-        content = {
-            item {
-                PeopleUserInput(onPeopleChanged = { sleepUpdates.onPeopleChanged })
-            }
-            item {
-                DatesUserInput(
-                    datesSelected,
-                    onDateSelectionClicked = sleepUpdates.onDateSelectionClicked
-                )
-            }
-            item {
-                SimpleUserInput(
-                    caption = stringResource(R.string.input_select_location),
-                    vectorImageId = R.drawable.ic_hotel
-                )
-            }
+
+    // Display the Crane search UI
+    CraneSearch(columns) {
+
+        item {
+            PeopleUserInput(onPeopleChanged = { sleepUpdates.onPeopleChanged })
         }
-    )
+
+        item {
+            DatesUserInput(
+                datesSelected,
+                onDateSelectionClicked = sleepUpdates.onDateSelectionClicked
+            )
+        }
+
+        item {
+            SimpleUserInput(
+                caption = stringResource(R.string.input_select_location),
+                vectorImageId = R.drawable.ic_hotel
+            )
+        }
+    }
 }
 
 @Composable
@@ -110,28 +104,35 @@ fun EatSearchContent(
     datesSelected: String,
     eatUpdates: EatSearchContentUpdates
 ) {
+    // Determine the number of columns based on window width
     val columns = when (widthSize) {
         WindowWidthSizeClass.Compact -> 1
         WindowWidthSizeClass.Medium -> 2
         WindowWidthSizeClass.Expanded -> 4
         else -> 1
     }
+
+    // Display the Crane search UI
     CraneSearch(columns) {
+        // People user input
         item {
             PeopleUserInput(onPeopleChanged = eatUpdates.onPeopleChanged)
         }
+        // Dates user input
         item {
             DatesUserInput(
                 datesSelected,
                 onDateSelectionClicked = eatUpdates.onDateSelectionClicked
             )
         }
+        // Time user input for eat
         item {
             SimpleUserInput(
                 caption = stringResource(R.string.input_select_time),
                 vectorImageId = R.drawable.ic_time
             )
         }
+        // Location user input for eat
         item {
             SimpleUserInput(
                 caption = stringResource(R.string.input_select_location),
@@ -146,6 +147,7 @@ private fun CraneSearch(
     columns: Int,
     content: LazyGridScope.() -> Unit
 ) {
+    // Display Crane search UI using LazyVerticalGrid
     LazyVerticalGrid(
         modifier = Modifier.padding(start = 24.dp, top = 0.dp, end = 24.dp, bottom = 12.dp),
         columns = GridCells.Fixed(columns),
--- app/src/main/java/androidx/compose/samples/crane/ui/CraneTheme.kt
+++ app/src/main/java/androidx/compose/samples/crane/ui/CraneTheme.kt
@@ -1,19 +1,3 @@
-/*
- * Copyright 2020 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package androidx.compose.samples.crane.ui
 
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -23,20 +7,21 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 
-val crane_caption = Color.DarkGray
+
+val crane_caption = Color.Gray
 val crane_divider_color = Color.LightGray
-private val crane_red = Color(0xFFE30425)
-private val crane_white = Color.White
-private val crane_purple_700 = Color(0xFF720D5D)
-private val crane_purple_800 = Color(0xFF5D1049)
-private val crane_purple_900 = Color(0xFF4E0D3A)
+
+
+val colorPrimary = Color(0xFF0088FF)
+val colorPrimaryDark = Color(0xFF215367)
+val colorAccent = Color(0xFF00B7FF)
 
 val craneColors = lightColors(
-    primary = crane_purple_800,
-    secondary = crane_red,
-    surface = crane_purple_900,
-    onSurface = crane_white,
-    primaryVariant = crane_purple_700
+    primary = colorPrimaryDark,
+    secondary = colorAccent,
+    surface = colorPrimary,
+    onSurface = Color.White,
+    primaryVariant = colorPrimary
 )
 
 val BottomSheetShape = RoundedCornerShape(
